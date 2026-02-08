4e3725762422e5a548ac8d639f965e3e6616f47b
linusfeng <linusfeng0@gmail.com>
2026-02-07 18:47:19 -0800

    Add personal introduction print statement
    Insert an additional print in labs/lab_1/lab_1a.py to output a brief personal introduction. The new line prints a short bio (name, hometown, school, activities and interests) immediately after the existing welcome message in main().

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Linus Feng" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print('Hi, my name is Linus, and I\'m a sophomore from San Mateo, California. I go to Aragon High School. One fun fact about me is that I play tennis and also play the oboe in my school band. I also like mechanical engineering, and I\'m on my school\'s robotics team')
 
 if __name__ == "__main__":
     main()
